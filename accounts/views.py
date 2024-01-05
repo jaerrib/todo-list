319223ee82d21bb362bc4344d13061fbe7e948bf
jaerrib <jaerrib@tutanota.com>
2024-01-05 12:19:08 -0500

    Update signup and login forms

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,10 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, TemplateView
 
-from .forms import CustomUserCreationForm
-
 
 class SignUpView(CreateView):
-    form_class = CustomUserCreationForm
+    form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
 
--- django_project/settings.py
+++ django_project/settings.py
@@ -122,5 +122,7 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+LOGIN_REDIRECT_URL = "home"
+
 CRISPY_ALLOWED_TEMPLATE_PACK = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
