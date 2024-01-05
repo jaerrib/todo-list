deca79bf55ee6523648534f1bae31fdd6bdb113c
jaerrib <jaerrib@tutanota.com>
2024-01-05 12:47:38 -0500

    Add password change and reset, update new ToDo form

--- django_project/settings.py
+++ django_project/settings.py
@@ -126,3 +126,5 @@ LOGIN_REDIRECT_URL = "home"
 
 CRISPY_ALLOWED_TEMPLATE_PACK = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
