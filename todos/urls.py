6d358a8f88c4a669f85696a269ab1db19b646595
jaerrib <jaerrib@tutanota.com>
2024-01-06 06:17:13 -0500

    Rewrite ToDo completion logic, update Details view

--- todos/urls.py
+++ todos/urls.py
@@ -7,7 +7,8 @@ from .views import (
     ToDoCreateView,
     ToDoUpdateView,
     TodoDeleteView,
-    ToDoCompleteView,
+    todo_complete,
+    todo_reactivate,
 )
 
 urlpatterns = [
@@ -17,7 +18,7 @@ urlpatterns = [
     path("todos/<int:pk>/edit/", ToDoUpdateView.as_view(), name="todo_edit"),
     path("todos/<int:pk>/delete/", TodoDeleteView.as_view(),
          name="todo_delete"),
-    path("todos/<int:pk>/complete/", ToDoCompleteView.as_view(),
-         name="todo_complete"),
+    path("todos/<int:pk>/complete/", todo_complete, name="todo_complete"),
+    path("todos/<int:pk>/reactivate/", todo_reactivate, name="todo_reactivate"),
     path("", HomePageView.as_view(), name="home"),
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView, ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
@@ -61,13 +62,29 @@ class TodoDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
         return obj.creator == self.request.user
 
 
-class ToDoCompleteView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
-    model = ToDo
-    template_name = "todo_complete.html"
-    fields = [
-        "completed",
-    ]
+def todo_complete(request, pk):
+    for key, value in request.session.items():
+        print("{} => {}".format(key, value))
+    if "_auth_user_id" not in request.session:
+        return redirect("/")
+    model = ToDo.objects.get(pk=pk)
+    if request.session["_auth_user_id"] != str(model.creator.pk):
+        return redirect("home")
+    else:
+        model.completed = True
+        model.save()
+        return redirect("todo_detail", pk=pk)
 
-    def test_func(self):
-        obj = self.get_object()
-        return obj.creator == self.request.user
+
+def todo_reactivate(request, pk):
+    for key, value in request.session.items():
+        print("{} => {}".format(key, value))
+    if "_auth_user_id" not in request.session:
+        return redirect("/")
+    model = ToDo.objects.get(pk=pk)
+    if request.session["_auth_user_id"] != str(model.creator.pk):
+        return redirect("home")
+    else:
+        model.completed = False
+        model.save()
+        return redirect("todo_detail", pk=pk)
