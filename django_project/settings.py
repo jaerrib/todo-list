a20227d17acfd3d5a0e27e1ffa1d5f9a62bf6f31
jaerrib <jaerrib@tutanota.com>
2024-01-07 15:01:42 -0500

    Update allowed hosts

--- django_project/settings.py
+++ django_project/settings.py
@@ -29,8 +29,13 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = ["eisenhower-todo-list.fly.dev ", "localhost", "127.0.0.1"]
-CSRF_TRUSTED_ORIGINS = ["https://eisenhower-todo-list.fly.dev "]
+ALLOWED_HOSTS = [
+    "eisenhower-todo-list.fly.dev ",
+    "localhost",
+    "127.0.0.1",
+    "0.0.0.0",
+]
+CSRF_TRUSTED_ORIGINS = ["https://eisenhower-todo-list.fly.dev"]
 
 # Application definition
 
