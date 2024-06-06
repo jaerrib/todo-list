5387aea7f84f13e7b68e5ef71c40214c3d3cdd2d
jaerrib <jaerrib@tutanota.com>
2024-06-06 13:18:49 -0400

    Add ability for users to delete their own accounts

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import SignUpView, LogOutRenderView
+from .views import SignUpView, LogOutRenderView, AccountDeleteView
 
 urlpatterns = [
     path("logout/", LogOutRenderView.as_view(), name="logout"),
     path("signup/", SignUpView.as_view(), name="signup"),
+    path("delete/<int:pk>/", AccountDeleteView.as_view(), name="account_delete"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, TemplateView
+from django.views.generic import CreateView, DeleteView, TemplateView
 
 
 class SignUpView(CreateView):
@@ -11,3 +13,17 @@ class SignUpView(CreateView):
 
 class LogOutRenderView(TemplateView):
     template_name = "home.html"
+
+
+class AccountDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = User
+    template_name = "registration/account_delete.html"
+    success_url = reverse_lazy("home")
+
+    def get_queryset(self):
+        query_set = User.objects.filter(pk=self.request.user.pk)
+        return query_set
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.pk == self.request.user.pk
